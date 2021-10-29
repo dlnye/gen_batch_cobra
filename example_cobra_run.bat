e3c28137b13a9179d2e30631bdf320f5c3bb0377
Leith Nye <leith@illumeadvising.com>
2021-10-29 08:29:16 -0500

    first commit

---
+++ example_cobra_run.bat
@@ -0,0 +1,112 @@
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2022.csv" --control "P:\cobra_gen_batch\control\s2022.csv" --output "P:\cobra_gen_batch\dr3pct\s2022.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2023.csv" --control "P:\cobra_gen_batch\control\s2023.csv" --output "P:\cobra_gen_batch\dr3pct\s2023.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2024.csv" --control "P:\cobra_gen_batch\control\s2024.csv" --output "P:\cobra_gen_batch\dr3pct\s2024.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2025.csv" --control "P:\cobra_gen_batch\control\s2025.csv" --output "P:\cobra_gen_batch\dr3pct\s2025.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2026.csv" --control "P:\cobra_gen_batch\control\s2026.csv" --output "P:\cobra_gen_batch\dr3pct\s2026.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2027.csv" --control "P:\cobra_gen_batch\control\s2027.csv" --output "P:\cobra_gen_batch\dr3pct\s2027.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2028.csv" --control "P:\cobra_gen_batch\control\s2028.csv" --output "P:\cobra_gen_batch\dr3pct\s2028.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2029.csv" --control "P:\cobra_gen_batch\control\s2029.csv" --output "P:\cobra_gen_batch\dr3pct\s2029.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2030.csv" --control "P:\cobra_gen_batch\control\s2030.csv" --output "P:\cobra_gen_batch\dr3pct\s2030.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2031.csv" --control "P:\cobra_gen_batch\control\s2031.csv" --output "P:\cobra_gen_batch\dr3pct\s2031.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2032.csv" --control "P:\cobra_gen_batch\control\s2032.csv" --output "P:\cobra_gen_batch\dr3pct\s2032.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2033.csv" --control "P:\cobra_gen_batch\control\s2033.csv" --output "P:\cobra_gen_batch\dr3pct\s2033.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2034.csv" --control "P:\cobra_gen_batch\control\s2034.csv" --output "P:\cobra_gen_batch\dr3pct\s2034.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2035.csv" --control "P:\cobra_gen_batch\control\s2035.csv" --output "P:\cobra_gen_batch\dr3pct\s2035.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2036.csv" --control "P:\cobra_gen_batch\control\s2036.csv" --output "P:\cobra_gen_batch\dr3pct\s2036.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2037.csv" --control "P:\cobra_gen_batch\control\s2037.csv" --output "P:\cobra_gen_batch\dr3pct\s2037.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2038.csv" --control "P:\cobra_gen_batch\control\s2038.csv" --output "P:\cobra_gen_batch\dr3pct\s2038.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2039.csv" --control "P:\cobra_gen_batch\control\s2039.csv" --output "P:\cobra_gen_batch\dr3pct\s2039.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2040.csv" --control "P:\cobra_gen_batch\control\s2040.csv" --output "P:\cobra_gen_batch\dr3pct\s2040.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2041.csv" --control "P:\cobra_gen_batch\control\s2041.csv" --output "P:\cobra_gen_batch\dr3pct\s2041.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2042.csv" --control "P:\cobra_gen_batch\control\s2042.csv" --output "P:\cobra_gen_batch\dr3pct\s2042.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2043.csv" --control "P:\cobra_gen_batch\control\s2043.csv" --output "P:\cobra_gen_batch\dr3pct\s2043.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2044.csv" --control "P:\cobra_gen_batch\control\s2044.csv" --output "P:\cobra_gen_batch\dr3pct\s2044.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2045.csv" --control "P:\cobra_gen_batch\control\s2045.csv" --output "P:\cobra_gen_batch\dr3pct\s2045.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2046.csv" --control "P:\cobra_gen_batch\control\s2046.csv" --output "P:\cobra_gen_batch\dr3pct\s2046.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2047.csv" --control "P:\cobra_gen_batch\control\s2047.csv" --output "P:\cobra_gen_batch\dr3pct\s2047.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2048.csv" --control "P:\cobra_gen_batch\control\s2048.csv" --output "P:\cobra_gen_batch\dr3pct\s2048.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra.db" --baseline "P:\cobra_gen_batch\baseline\s2049.csv" --control "P:\cobra_gen_batch\control\s2049.csv" --output "P:\cobra_gen_batch\dr3pct\s2049.csv" --pct3 yes --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2022.csv" --control "P:\cobra_gen_batch\control\s2022.csv" --output "P:\cobra_gen_batch\dr7pct\s2022.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2023.csv" --control "P:\cobra_gen_batch\control\s2023.csv" --output "P:\cobra_gen_batch\dr7pct\s2023.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2024.csv" --control "P:\cobra_gen_batch\control\s2024.csv" --output "P:\cobra_gen_batch\dr7pct\s2024.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2025.csv" --control "P:\cobra_gen_batch\control\s2025.csv" --output "P:\cobra_gen_batch\dr7pct\s2025.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2026.csv" --control "P:\cobra_gen_batch\control\s2026.csv" --output "P:\cobra_gen_batch\dr7pct\s2026.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2027.csv" --control "P:\cobra_gen_batch\control\s2027.csv" --output "P:\cobra_gen_batch\dr7pct\s2027.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2028.csv" --control "P:\cobra_gen_batch\control\s2028.csv" --output "P:\cobra_gen_batch\dr7pct\s2028.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2029.csv" --control "P:\cobra_gen_batch\control\s2029.csv" --output "P:\cobra_gen_batch\dr7pct\s2029.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2030.csv" --control "P:\cobra_gen_batch\control\s2030.csv" --output "P:\cobra_gen_batch\dr7pct\s2030.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2031.csv" --control "P:\cobra_gen_batch\control\s2031.csv" --output "P:\cobra_gen_batch\dr7pct\s2031.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2032.csv" --control "P:\cobra_gen_batch\control\s2032.csv" --output "P:\cobra_gen_batch\dr7pct\s2032.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2033.csv" --control "P:\cobra_gen_batch\control\s2033.csv" --output "P:\cobra_gen_batch\dr7pct\s2033.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2034.csv" --control "P:\cobra_gen_batch\control\s2034.csv" --output "P:\cobra_gen_batch\dr7pct\s2034.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2035.csv" --control "P:\cobra_gen_batch\control\s2035.csv" --output "P:\cobra_gen_batch\dr7pct\s2035.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2036.csv" --control "P:\cobra_gen_batch\control\s2036.csv" --output "P:\cobra_gen_batch\dr7pct\s2036.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2037.csv" --control "P:\cobra_gen_batch\control\s2037.csv" --output "P:\cobra_gen_batch\dr7pct\s2037.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2038.csv" --control "P:\cobra_gen_batch\control\s2038.csv" --output "P:\cobra_gen_batch\dr7pct\s2038.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2039.csv" --control "P:\cobra_gen_batch\control\s2039.csv" --output "P:\cobra_gen_batch\dr7pct\s2039.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2040.csv" --control "P:\cobra_gen_batch\control\s2040.csv" --output "P:\cobra_gen_batch\dr7pct\s2040.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2041.csv" --control "P:\cobra_gen_batch\control\s2041.csv" --output "P:\cobra_gen_batch\dr7pct\s2041.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2042.csv" --control "P:\cobra_gen_batch\control\s2042.csv" --output "P:\cobra_gen_batch\dr7pct\s2042.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2043.csv" --control "P:\cobra_gen_batch\control\s2043.csv" --output "P:\cobra_gen_batch\dr7pct\s2043.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2044.csv" --control "P:\cobra_gen_batch\control\s2044.csv" --output "P:\cobra_gen_batch\dr7pct\s2044.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2045.csv" --control "P:\cobra_gen_batch\control\s2045.csv" --output "P:\cobra_gen_batch\dr7pct\s2045.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2046.csv" --control "P:\cobra_gen_batch\control\s2046.csv" --output "P:\cobra_gen_batch\dr7pct\s2046.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2047.csv" --control "P:\cobra_gen_batch\control\s2047.csv" --output "P:\cobra_gen_batch\dr7pct\s2047.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2048.csv" --control "P:\cobra_gen_batch\control\s2048.csv" --output "P:\cobra_gen_batch\dr7pct\s2048.csv" --pct3 no --year 2023
+
+"C:\Program Files\COBRA\cobra_console.exe" --db "C:\Program Files\COBRA\data\cobra7pct.db" --baseline "P:\cobra_gen_batch\baseline\s2049.csv" --control "P:\cobra_gen_batch\control\s2049.csv" --output "P:\cobra_gen_batch\dr7pct\s2049.csv" --pct3 no --year 2023
+
