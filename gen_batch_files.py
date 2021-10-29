5b06e561780dc0cec6a3e3af38a9c6142374b4b1
Leith Nye <leith@illumeadvising.com>
2021-10-29 08:34:10 -0500

    minor formatting changes and documentation added

--- gen_batch_files.py
+++ gen_batch_files.py
@@ -1,34 +1,52 @@
 # -*- coding: utf-8 -*- 
 
-#%%
+
+'''
+Script to generate batch file to feed multiple runs of the EPA 
+CO-Benefits Risk Assessment Health Impacts Screening and Mapping Tool (COBRA)
+with different county-level emissions data.
+COBRA Tool url:
+https://www.epa.gov/cobra
+
+This makes it easier to estimate cumulative emissions reduction health impacts
+for a series of years and/or different discount rates, for example.
+
+For more information about COBRA's batch file capability see the user manual,
+Appendix G:
+
+https://www.epa.gov/sites/default/files/2018-05/documents/cobra_user_manual_may2018_508.pdf
+
+NOTE: This is could be refactored and made into a function but this was a one-off task so 
+I did not do this.
+'''
+
+
 import os
 
 
-#%%
-
-#%%
 # Variables:
 # edit these based on the file you want to generate
 cd = os.getcwd()
 
+# example input data file name: baseline\s2022.csv"
+# location of the baseline emissions data
 baseline_dir = os.path.join(cd,"baseline")
+# location of the control emissions data
 control_dir = os.path.join(cd,"control")
+# directories where you want COBRA to save output
+# under different discount rates
 results_dir_3pct =  os.path.join(cd,"dr3pct") 
 results_dir_7pct =  os.path.join(cd,"dr7pct") 
-
-batch_file = "cobra_run.bat" 
-
+# name of the batch file to output from this script
+batch_file = "example_cobra_run.bat" 
+# optional: prefix for input file names:
 file_prefix = 's'
 
-#%%
-
-#%%
-
-
-#%%
-
+# start and end years for the emissions files
+# these are part of the emission file names
 start_year = 2022
 end_year = 2049
+
 pop_incidence_year = 2023 # Options: 2016, 2023, 2028
 use_closest_default = False
 
@@ -54,7 +72,7 @@ nyrs = len(yr_range)
 file_names = [file_prefix +i+'.csv' for i in yr_range]
 
 
-# this is clunky but each file path needs to be formatted with double quotes
+# this is clunky but each file path needs to be formatted with double quotes for Windows
 
 blfp = [baseline_dir+"\\"+i for i in file_names]
 blfp = ['"{}"'.format(i) for i in blfp]
